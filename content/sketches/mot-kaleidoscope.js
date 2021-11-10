405a7d790d4cb92b7be5f620098d96575c76788b
Francisco Dueñas <frsduenasca@unal.edu.co>
2021-11-10 15:55:17 -0500

    :zap: New canvas with a circle

---
+++ content/sketches/mot-kaleidoscope.js
@@ -0,0 +1,9 @@
+function setup() {
+    // Sets the screen to be 720 pixels wide and 400 pixels high
+    createCanvas(400, 400);
+    background(0);
+
+    fill(255);
+    ellipse(252, 144, 72, 72);
+
+  }
