3ecd12ad8f301bbdbb7b1db6071457be68b75d99
Francisco Dueñas <frsduenasca@unal.edu.co>
2021-11-10 22:45:13 -0500

    :zap: Kaleidoscope is working!

--- content/sketches/mot-kaleidoscope.js
+++ content/sketches/mot-kaleidoscope.js
@@ -12,39 +12,36 @@ function draw() {
   background(255);
   noFill();
 
-  // The two parameters of the point() method each specify
-  // coordinates.
-  // The first parameter is the x-coordinate and the second is the Y
-  stroke(255);
-  point(width * 0.5, height * 0.5);
+  // Rotate section
+  for(let i = radius; i < radius + gap; i++ ){
+    push();
+    stroke(255, 0, 0);
+    translate(width * 0.5, height * 0.5);
+    rotate(frameCount / 200.0);
+    star(0, 0, i , i + gap, npoints);
+    pop();
+  }
 
   // Inside section
   push();
   stroke(0, 62, 107);
-  fill(0, 62, 107);
+  fill(0, 62, 107, 255);
   translate(width * 0.5, height * 0.5);
-  star(0, 0, 0, 0, radius, radius + gap, npoints);
+  star(0, 0, radius, radius + gap, npoints);
   pop();
 
   // Outside section
-  push();
-  stroke(0, 62, 107);
-  translate(width * 0.5, height * 0.5);
-  star(0, 0, radius + gap, radius + (gap * 2), radius + (gap * 2), radius + (gap * 3), npoints);
-  pop();
-
-  // Rotate section
-  
-  push();
-  stroke(255, 0, 0);
-  translate(width * 0.5, height * 0.5);
-  rotate(frameCount / 200.0);
-  star(0, 0, radius , radius + gap, radius + gap, radius + (gap * 2), npoints);
-  pop();
+  for(let i = radius; i < radius + gap; i++ ){
+    push();
+    stroke(0, 62, 107);
+    translate(width * 0.5, height * 0.5);
+    star(0, 0, i + gap, i + (gap * 2), npoints);
+    pop();
+  }
 
 }
 
-function star(x, y, radius1, radius2, radius3, radius4, npoints) {
+function star(x, y, radius1, radius2,npoints) {
   let angle = TWO_PI / npoints;
   let halfAngle = angle / 2.0;
   beginShape();
@@ -54,14 +51,6 @@ function star(x, y, radius1, radius2, radius3, radius4, npoints) {
     vertex(sx, sy);
     sx = x + cos(a + halfAngle) * radius1;
     sy = y + sin(a + halfAngle) * radius1;
-    vertex(sx, sy);
-  }
-  for (let a = 0; a < TWO_PI; a += angle) {
-    let sx = x + cos(a) * radius4;
-    let sy = y + sin(a) * radius4;
-    vertex(sx, sy);
-    sx = x + cos(a + halfAngle) * radius3;
-    sy = y + sin(a + halfAngle) * radius3;
     vertex(sx, sy);
   }
   endShape(CLOSE);
