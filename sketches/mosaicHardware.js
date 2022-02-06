3de11d990eafbd737c9869046783b89d46fd831d
LeronArenwino <LeronArenwino@users.noreply.github.com>
2022-02-06 06:43:53 +0000

    deploy: 40f73cb6741336fe87bbd9bdf7459dc8bc5ed846

--- sketches/mosaicHardware.js
+++ sketches/mosaicHardware.js
@@ -1,101 +1,49 @@
-// Variables
-let imageCells;
-let pg;
-let mosaic;
-let video_src;
-let debugging;
-let cols;
-
-// User Interface
-let resolution;
-let sel;
-let video_on;
-let p;
-
-const SAMPLE_RES = 30;
+let mosaicImages;
+let indexImages;
+let theShader;
+let img;
 
 function preload() {
-  video_src = createVideo(['sketches/fingers.webm']);
-  video_src.hide();
-
-  mosaic = readShader('/sketches/mosaicShader.frag');
-
-  p = [];
-  for (let i = 1; i <= 4; i++) {
-    p.push(loadImage(`/sketches/images/p${i}.jpg`));
-  }
+  theShader = loadShader(
+    '/vc/docs/sketches/shader.vert',
+    '/vc/docs/sketches/mosaicShader.frag',
+  );
+  img = loadImage('/vc/docs/sketches/lenna.png');
+  mosaicImages = loadImage('/vc/docs/sketches/mosaicImages.jpg');
+  indexImages = loadImage('/vc/docs/sketches/indexImage.jpg');
 }
 
 function setup() {
-  createCanvas(650, 650, WEBGL);
-  colorMode(RGB, 1);
-  imageCells = createQuadrille(p);
-  textureMode(NORMAL);
+  createCanvas(512, 512, WEBGL);
   noStroke();
-  shader(mosaic);
-  sel = createSelect();
-  sel.position(10, 125);
-  sel.option('Keys');
-  sel.option('Symbols');
-  sel.selected('Symbols');
+  textureMode(NORMAL);
+  shader(theShader);
 
-  sel.changed(() => {
-    mosaic.setUniform('debug', sel.value() === 'Keys');
-    mosaic.setUniform('color_on', false);
+  theShader.setUniform('image', img);
+  theShader.setUniform('mosaicImages', mosaicImages);
+  theShader.setUniform('indexImages', indexImages);
+  theShader.setUniform('resolution', 20);
+
+  slider = createSlider(3, 512, 100, 2);
+  slider.position(150, 552);
+  slider.style('width', '40%');
+  slider.input(() => {
+    theShader.setUniform('resolution', slider.value());
+    redraw();
   });
-
-  video_on = createCheckbox('Video', false);
-  video_on.style('color', 'magenta');
-  video_on.changed(() => {
-    if (video_on.checked()) {
-      mosaic.setUniform('source', video_src);
-      video_src.loop();
-    } else {
-      mosaic.setUniform('source', random(p));
-      video_src.pause();
-    }
-  });
-  video_on.position(10, 80);
-  mosaic.setUniform('source', random(p));
-  resolution = createSlider(10, 200, SAMPLE_RES, 1);
-  resolution.position(10, 100);
-  resolution.style('width', '80px');
-  resolution.input(() => {
-    mosaic.setUniform('resolution', resolution.value());
-  });
-
-  mosaic.setUniform('resolution', resolution.value());
-  pg = createGraphics(SAMPLE_RES * imageCells.width, SAMPLE_RES);
-
-  mosaic.setUniform('cols', imageCells.width);
-  sample();
-}
-
-function keyPressed() {
-  if (key === 'r' && !video_on.checked()) {
-    mosaic.setUniform('source', random(p));
-  }
-}
-
-function sample() {
-  if (pg.width !== SAMPLE_RES * imageCells.width) {
-    pg = createGraphics(SAMPLE_RES * imageCells.width, SAMPLE_RES);
-    mosaic.setUniform('cols', imageCells.width);
-  }
-
-  imageCells.sort({ ascending: true, cellLength: SAMPLE_RES });
-  drawQuadrille(imageCells, {
-    graphics: pg,
-    cellLength: SAMPLE_RES,
-    outlineWeight: 0,
-  });
-  mosaic.setUniform('palette', pg);
+  let div = createDiv('Resolution');
+  div.style('font-size', '18px');
+  div.position(220, 532);
 }
 
 function draw() {
-  cover({ texture: true });
-}
+  background(0);
+  beginShape();
+  vertex(-width / 2, -height / 2, 0, 0, 0);
+  vertex(width / 2, -height / 2, 0, 1, 0);
+  vertex(width / 2, height / 2, 0, 1, 1);
+  vertex(-width / 2, height / 2, 0, 0, 1);
+  endShape(CLOSE);
 
-function windowResided() {
-  resizeCanvas(windowWidth, windowHeight);
+  noLoop();
 }
